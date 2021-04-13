707f315ca6d8496a9d148c7ebc7f90912a4e8941
Rois <rois.khoiron@gmail.com>
2021-04-13 09:00:56 +0700

    Final commit

---
+++ app/src/main/java/com/example/alarmmanager/DatePickerFragment.java
@@ -0,0 +1,63 @@
+package com.example.alarmmanager;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    DialogDateListener mListener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context != null) {
+            mListener = (DialogDateListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        if (mListener != null) {
+            mListener = null;
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
+        return new DatePickerDialog(getActivity(), this, year, month, date);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        mListener.onDialogDataSet(getTag(), year, month, dayOfMonth);
+    }
+
+    public interface DialogDateListener {
+        void onDialogDataSet(String tag, int year, int month, int dayOfMonth);
+    }
+}
